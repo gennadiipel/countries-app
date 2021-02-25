7d22708c252c3a16b7e1e1d17a0861cd58547abb
pelhenry <thedankertanker@outlook.com>
2021-02-25 17:43:46 +0200

    project structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListComponent } from './components/list/list.component';
+import { CountryCardComponent } from './components/country-card/country-card.component';
+import { CountryComponent } from './components/country/country.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListComponent,
+    CountryCardComponent,
+    CountryComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
